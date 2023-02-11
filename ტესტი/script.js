8c7ed8e85d46485e4b9bd251f654354531ce889a
giorgiGrigolia <grigolialive@gmail.com>
2023-02-12 01:56:48 +0400

    Redesigned the structure of the personal information page

--- ტესტი/script.js
+++ ტესტი/script.js
@@ -33,14 +33,25 @@ document.querySelector("#resumeAboutMe").innerHTML =
 
 document.querySelector("#INF-aboutMe").value =
   sessionStorage.getItem("INF-aboutMe");
+
+document.querySelector("#resumeMail").innerHTML =
+  sessionStorage.getItem("INF-email");
+
+document.querySelector("#INF-email").value =
+  sessionStorage.getItem("INF-email");
+
+document.querySelector("#resumeNumber").innerHTML =
+  sessionStorage.getItem("INF-mobileNumber");
+
+document.querySelector("#INF-mobileNumber").value =
+  sessionStorage.getItem("INF-mobileNumber");
 //#endregion
 //#region CALL ERROR / SUCCESS FUNCTIONS
 errorSuccessHandlingForFirstName();
 errorSuccessHandlingForLastName();
 errorSuccessHandlingForAboutMe();
-//#endregion
-//#region DISPLAYING / HIDING ICONS
-
+errorSuccessHandlingForEmail();
+errorSuccessHandlingForNumber();
 //#endregion
 //#region onInput functions
 function firstNameOnInput() {
@@ -75,6 +86,75 @@ function aboutMeOnInput() {
   //ERROR / SUCCESS HANDLING
   errorSuccessHandlingForAboutMe();
 }
+
+function emailOnInput() {
+  //reading the value
+  email = document.querySelector("#INF-email").value;
+  //syncing the data with resume divs
+  document.querySelector("#resumeMail").innerHTML = email;
+  //storing the values into session
+  sessionStorage.setItem("INF-email", email);
+  //ERROR / SUCCESS HANDLING
+  errorSuccessHandlingForEmail();
+}
+
+function numberOnInput() {
+  //reading the value
+  mobileNumber = document.querySelector("#INF-mobileNumber").value;
+  //syncing the data with resume divs
+  document.querySelector("#resumeNumber").innerHTML = mobileNumber;
+  //storing the values into session
+  sessionStorage.setItem("INF-mobileNumber", mobileNumber);
+  //ERROR / SUCCESS HANDLING
+  errorSuccessHandlingForNumber();
+}
+//#endregion
+//#region image handling
+const uploadedImage = document.querySelector("#INF-image");
+const previewImage = document.querySelector("#previewImage");
+const imagePass = document.querySelector(".passImage");
+const imageError = document.querySelector(".errorImage");
+
+uploadedImage.addEventListener("change", function () {
+  if (uploadedImage.value) {
+    imagePass.style.display = "block";
+    imageError.style.display = "none";
+  }
+});
+
+uploadedImage.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const objectURL = URL.createObjectURL(file);
+    previewImage.src = objectURL;
+  }
+});
+
+window.addEventListener("load", function () {
+  if (this.sessionStorage.getItem("INF-image")) {
+    previewImage.src = this.sessionStorage.getItem("INF-image");
+    imagePass.style.display = "block";
+    imageError.style.display = "none";
+    previewImage.style.display = "block";
+  }
+
+  this.document
+    .querySelector("#INF-image")
+    .addEventListener("change", function () {
+      const file = this.files[0];
+      if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+          previewImage.src = reader.result;
+          previewImage.style.display = "block";
+          sessionStorage.setItem("INF-image", reader.result);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    });
+});
 //#endregion
 //#region ERROR / SUCCESS HANDLING
 function errorSuccessHandlingForFirstName() {
@@ -88,6 +168,7 @@ function errorSuccessHandlingForFirstName() {
     firstNamePass.style.display = "block";
     firstNameError.style.display = "none";
     document.querySelector(".redFName").style.color = "#1A1A1A";
+    firstNameValidated = true;
   } else if (firstNameElement.value === "") {
     firstNamePass.style.display = "none";
     firstNameError.style.display = "none";
@@ -111,6 +192,7 @@ function errorSuccessHandlingForLastName() {
     lastNamePass.style.display = "block";
     lastNameError.style.display = "none";
     document.querySelector(".redLName").style.color = "#1A1A1A";
+    lastNameValidated = true;
   } else if (lastNameElement.value === "") {
     lastNamePass.style.display = "none";
     lastNameError.style.display = "none";
@@ -130,10 +212,83 @@ function errorSuccessHandlingForAboutMe() {
 
   if (aboutMeElement.value !== "") {
     aboutMeElement.style.border = "1px solid #98E37E";
-    document.querySelector(".resumeAboutMeHeader").style.display = "inline";
+    document.querySelector(".resumeAboutMeHeader").style.display = "block";
   } else {
     aboutMeElement.style.border = "1px solid #BCBCBC";
     document.querySelector(".resumeAboutMeHeader").style.display = "none";
+  }
+}
+
+function errorSuccessHandlingForEmail() {
+  //reading the element
+  let emailElement = document.querySelector("#INF-email");
+  if (emailElement.value !== "") {
+    document.querySelector("#resumeEmailLogo").style.display = "block";
+  } else {
+    document.querySelector("#resumeEmailLogo").style.display = "none";
+  }
+
+  if (emailElement.value.match(/[a-zA-Z0-9._%+-]+@redberry\.ge$/)) {
+    emailPass.style.display = "block";
+    emailError.style.display = "none";
+    emailElement.style.border = "1px solid #98E37E";
+    document.querySelector(".redEmail").style.color = "#1A1A1A";
+    emailValidated = true;
+  } else if (emailElement.value === "") {
+    emailPass.style.display = "none";
+    emailError.style.display = "none";
+    emailElement.style.border = "1px solid #bcbcbc";
+    document.querySelector(".redEmail").style.color = "#1A1A1A";
+  } else {
+    emailPass.style.display = "none";
+    emailError.style.display = "block";
+    emailElement.style.border = "1px solid #EF5050";
+    document.querySelector(".redEmail").style.color = "#E52F2F";
+  }
+}
+
+function errorSuccessHandlingForNumber() {
+  //reading the element
+  let numberElement = document.querySelector("#INF-mobileNumber");
+  if (numberElement.value !== "") {
+    document.querySelector("#resumePhoneLogo").style.display = "block";
+  } else {
+    document.querySelector("#resumePhoneLogo").style.display = "none";
+  }
+
+  if (numberElement.value === "") {
+    mobileNumberPass.style.display = "none";
+    mobileNumberError.style.display = "none";
+    numberElement.style.border = "1px solid #bcbcbc";
+    document.querySelector(".redPhoneNumber").style.color = "#1A1A1A";
+  } else if (
+    numberElement.value.match(
+      /\+995\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}/
+    ) &&
+    numberElement.value.length === 17
+  ) {
+    mobileNumberPass.style.display = "block";
+    mobileNumberError.style.display = "none";
+    numberElement.style.border = "1px solid #98E37E";
+    document.querySelector(".redPhoneNumber").style.color = "#1A1A1A";
+  } else {
+    mobileNumberPass.style.display = "none";
+    mobileNumberError.style.display = "block";
+    numberElement.style.border = "1px solid #EF5050";
+    document.querySelector(".redPhoneNumber").style.color = "#E52F2F";
+  }
+}
+//#endregion
+//#region NEXT BUTTON
+function goNext() {
+  if (
+    firstNamePass.style.display === "block" &&
+    lastNamePass.style.display === "block" &&
+    emailPass.style.display === "block" &&
+    mobileNumberPass.style.display === "block" &&
+    imagePass.style.display === "block"
+  ) {
+    window.location.href = "../გამოცდილება/index.html";
   }
 }
 //#endregion
